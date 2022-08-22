b766fb620785a0c4df86d874f5a5100a46f9b380
tahir <tahir@DESKTOP-NFV9VCC>
2022-08-22 21:30:18 +0300

    initial

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSehir = new System.Windows.Forms.TextBox();
+            this.txtPlaka = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Citys = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Plaka = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Citys,
+            this.Plaka});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 135);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1109, 381);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.dataGridView1_RowsAdded);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(651, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Tıkla";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtSehir
+            // 
+            this.txtSehir.Location = new System.Drawing.Point(452, 37);
+            this.txtSehir.Name = "txtSehir";
+            this.txtSehir.Size = new System.Drawing.Size(100, 20);
+            this.txtSehir.TabIndex = 2;
+            // 
+            // txtPlaka
+            // 
+            this.txtPlaka.Location = new System.Drawing.Point(626, 36);
+            this.txtPlaka.Name = "txtPlaka";
+            this.txtPlaka.Size = new System.Drawing.Size(100, 20);
+            this.txtPlaka.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(400, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Şehir";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(569, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Koli";
+            // 
+            // Citys
+            // 
+            this.Citys.HeaderText = "Şehirler";
+            this.Citys.Name = "Citys";
+            // 
+            // Plaka
+            // 
+            this.Plaka.HeaderText = "Koli";
+            this.Plaka.Name = "Plaka";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1133, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPlaka);
+            this.Controls.Add(this.txtSehir);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Koli Uygulaması";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSehir;
+        private System.Windows.Forms.TextBox txtPlaka;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Citys;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Plaka;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           string[] datas=new string[] { "samandıra","esenyurt -1-","nilüfer","trakya - çorlu","kemalpaşa","sakarya","güzelhisar","esenyurt -3-","samsun","kocayatak","adana","trabzon","konya","şanlıurfa","aydın","kayseri","gebze -1-","balıkesir","akyurt -1-","gebze -2-","erzurum","çorum","haramidere","esenyurt -2-","gaziantep","ulukent","eskişehir","sancaktepe","yenikent","düzce","diyarbakır","muğla","kestel","van","mersin","aksaray","döşemealtı","ayrancılar","esenkent 1","ömerli","afyon","malatya","sivas","lüleburgaz","tuzla","gaziantep 2","iskenderun","kemalpaşa 2","hasanoğlan1","esenkent2","adana2","hasanoğlan2","","çumra","piraziz","kastamonu","denizli","izmit","batman","ığdır","arnavutköy 1","arnavutköy 2","döşemealtı 2","ısparta","osb ankara","akhisar","mersin 2","elazıg" };
+           int[] datas2 = new int[] {154,130,142,175,150,160,155,128,191,155,220,160,160,173,160,190,172,120,164,142,166,154,150,150,161,143,83,159,125,236,180,159,180,152,140,184,150,150,167,180,159,200,160,164,165,164,75,100,143,173,160,100,167,172,145,145,180,148,61,220,136,150,135,140,180,150,10 };
+
+            for (int i = 0; i < datas.Length -1; i++)
+            {
+                dataGridView1.Rows.Add(datas[i], datas2[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sehir = txtSehir.Text;
+            int plaka =Convert.ToInt32(txtPlaka.Text);
+
+
+            if(string.IsNullOrEmpty(sehir) || plaka==0)
+            {
+                MessageBox.Show("Hiçbir alanı boş bırakmayınız.");
+            }
+            else
+            {
+                dataGridView1.Rows.Add(sehir, plaka);
+                MessageBox.Show("Satır eklendi.");
+            }
+
+            
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+        }
+    }
+}
